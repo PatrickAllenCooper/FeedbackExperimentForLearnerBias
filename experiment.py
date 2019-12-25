afec08200fa84fc88584707643e08438a71f6ae4
Patrick Allen Cooper <patrick.allen.cooper@gmail.com>
2019-12-25 11:49:35 -0700

    Distributions visible general method devised. Operational assumptions must be introduced.

---
+++ experiment.py
@@ -0,0 +1,9 @@
+#########################
+# configures and executes
+# the experiment
+#########################
+import patient
+
+some_patient = patient.Patient()
+
+some_patient.define_meta_variables()
--- patient.py
+++ patient.py
@@ -5,11 +5,23 @@
 #########################
 
 from scipy.stats import gamma
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 class Patient:
+
     def __init__(self):
         self.DRUG_IMMUNITY = "gamma"
 
-
     def define_meta_variables(self):
+        data_gamma = gamma.rvs(a=5, size=10000)
         if self.DRUG_IMMUNITY is "gamma":
+            ax = sns.distplot(data_gamma,
+                              kde=True,
+                              bins=100,
+                              color='skyblue',
+                              hist_kws={"linewidth": 15, 'alpha': 1})
+            ax.set(xlabel='Gamma Distribution', ylabel='Frequency')
+
+            plt.show()
